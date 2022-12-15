97d85b216d429cac07f2227986e2fc2b0a14327c
Vlad <sekret.krut@gmail.com>
2022-12-15 16:51:58 +0200

    Added tests and xdebug

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ APP = gpr-reports-api
 
 .PHONY: migrate
 migrate:
-	docker-compose exec $(APP) php artisan migrate
+	docker exec $(APP) php artisan migrate
 
 .PHONY: install
 install:
-	docker-compose exec $(APP) composer install
+	docker exec $(APP) composer install
 
 .PHONY: bash
 bash:
--- tests/Feature/InvolvementTest.php
+++ tests/Feature/InvolvementTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Involvement;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -59,6 +60,156 @@ class InvolvementTest extends TestCase
             ->assertJson([
                 'data' => [
                     'save' => true
+                ]
+            ]);
+
+        $this->assertDatabaseCount('involvements', 1);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get_all_involvement(): void
+    {
+        $data = Involvement::factory()->count(3)->create();
+
+        $response = $this->getJson('api/all-involvement?filter[order]=id');
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    'type' => 'report',
+                    'attributes' => [
+                        [
+                            'id' => $data[0]->id,
+                            'act_code' => $data[0]->act_code,
+                            'report_code' => $data[0]->report_code,
+                            'date_notification' => $data[0]->date_notification,
+                            'task_type' => $data[0]->task_type,
+                            'place_execution' => $data[0]->place_execution,
+                            'examined' => $data[0]->examined
+                        ],
+                        [
+                            'id' => $data[1]->id,
+                            'act_code' => $data[1]->act_code,
+                            'report_code' => $data[1]->report_code,
+                            'date_notification' => $data[1]->date_notification,
+                            'task_type' => $data[1]->task_type,
+                            'place_execution' => $data[1]->place_execution,
+                            'examined' => $data[1]->examined
+                        ],
+                        [
+                            'id' => $data[2]->id,
+                            'act_code' => $data[2]->act_code,
+                            'report_code' => $data[2]->report_code,
+                            'date_notification' => $data[2]->date_notification,
+                            'task_type' => $data[2]->task_type,
+                            'place_execution' => $data[2]->place_execution,
+                            'examined' => $data[2]->examined
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseCount('involvements', 3);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get_involvement(): void
+    {
+        $data = Involvement::factory()->create();
+
+        $response = $this->getJson('api/involvement/' . $data->id);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    'type' => 'involvement',
+                    'attributes' => [
+                        'act_code' => $data->act_code,
+                        'report_code' => $data->report_code,
+                        'date_notification' => $data->date_notification,
+                        'date_received' => $data->date_received,
+                        'start_date' => $data->start_date,
+                        'end_date' => $data->end_date,
+                        'task_type' => $data->task_type,
+                        'work_status' => $data->work_status,
+                        'place_execution' => $data->place_execution,
+                        'examined' => $data->examined,
+                        'all_ammunition' => $data->all_ammunition,
+                        'tnt' => $data->tnt,
+                        'detonator' => $data->detonator
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseCount('involvements', 1);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_delete_involvement(): void
+    {
+        $data = Involvement::factory()->create();
+
+        $response = $this->deleteJson('api/involvement/' . $data->id);
+
+        $response
+            ->assertNoContent();
+
+        $this->assertDatabaseCount('involvements', 0);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_patch_involvement(): void
+    {
+        $data = Involvement::factory()->create();
+
+        $response = $this->patchJson('api/involvement/' . $data->id,
+            ['data' => [
+                'type' => 'report',
+                'attributes' => [
+                    'act_code' => $data->act_code,
+                    'report_code' => $data->report_code,
+                    'date_notification' => '2022-12-12',
+                    'date_received' => '2022-12-12 01:21',
+                    'start_date' => '2022-12-12 01:21',
+                    'end_date' => '2022-12-12 01:21',
+                    'task_type' => '2022-12-12 01:21',
+                    'work_status' => $data->work_status,
+                    'place_execution' => $data->place_execution,
+                    'coordinates' => json_decode($data->coordinates),
+                    'examined' => 0.93,
+                    'persons' => json_decode($data->persons),
+                    'ammunition' => json_decode($data->ammunition),
+                    'all_ammunition' => $data->all_ammunition,
+                    'tnt' => 21,
+                    'detonator' => 22
+                    ]
+                ]
+            ]
+        );
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    'update' => true
                 ]
             ]);
 
